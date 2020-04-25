c1d18aed82f462fff2709389985e508ba9dafc3e
Rotem <tetch6@gmail.com>
2020-04-25 21:45:11 +0300

    Updates

---
+++ precache-manifest.3791ffa5822f1d8528e167ae56e08b47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a472ceb9f9db6872978543066a65dc",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/index.html"
+  },
+  {
+    "revision": "d6760c3f5d31a471cd78",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/css/main.db8702be.chunk.css"
+  },
+  {
+    "revision": "5ed2e146912dff494b5f",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/js/2.05d0192c.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/js/2.05d0192c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6760c3f5d31a471cd78",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/js/main.8e6a4e41.chunk.js"
+  },
+  {
+    "revision": "92edc22af988b7a448cd",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/js/runtime-main.c1edaba3.js"
+  },
+  {
+    "revision": "a7803ddf902b6c7b5ac326af14e450b3",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/media/FugazOne-Regular.a7803ddf.ttf"
+  },
+  {
+    "revision": "d4f1d6fb0aba2b2244b788865d04f1dc",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/media/coins.d4f1d6fb.png"
+  },
+  {
+    "revision": "80e1c567a4884ba140a147ef704931c4",
+    "url": "/RotemCarmon/Mr.Bitcoin-react/static/media/usd.80e1c567.png"
+  }
+]);
